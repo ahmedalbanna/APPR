05d0bf5d488ef6f3a13f17e5cc0b57167bfda7d5
Ahmed Al-Banna <37414936+ahmedalbanna@users.noreply.github.com>
2023-07-25 21:13:47 +0300

    Initial commit

---
+++ src/screens/Box/Box.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+export const Box = () => {
+  return (
+    <div className="box">
+      <div className="APP-wrapper">
+        <img
+          className="APP"
+          alt="App"
+          src="https://generation-sessions.s3.amazonaws.com/bfd27f4d48e36b7198df95c9bcb92cd2/img/app.svg"
+        />
+      </div>
+    </div>
+  );
+};
